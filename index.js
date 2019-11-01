969e5607057fe818c658db8740d15f775ebd6eb7
Sophia Koeut <koeutsophia@gmail.com>
2019-11-01 10:32:29 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+function getDogImages (num) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
+    .then(response => response.json())
+    .then(responseJson =>
+        displayDogs(responseJson));
+}
+
+function displayDogs (dogsArray) {
+    console.log(dogsArray);
+    let dogHtml = '';
+    dogsArray.message.forEach(element => {
+        dogHtml += `<img class="results-img" src=${element}></img>`
+    });
+    console.log(dogHtml);
+    $('.image-container').html(dogHtml);
+}
+
+function submitHandler () {
+    $('form').submit(event => {
+        event.preventDefault();
+        let num = $('.numInput').val();
+        getDogImages(num);
+
+    
+    })
+}
+
+submitHandler();
